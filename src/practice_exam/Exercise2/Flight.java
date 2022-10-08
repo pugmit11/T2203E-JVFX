b40985a0da9628a7e8dc0f704db0fbfd15666c87
pugmit11 <tanhtanh1997@gmail.com>
2022-10-08 20:04:41 +0700

    ok

--- src/practice_exam/Exercise2/Flight.java
+++ src/practice_exam/Exercise2/Flight.java
@@ -1,4 +1,28 @@
 package practice_exam.Exercise2;
 
 public class Flight {
+    private int number;
+    private  String destination;
+    public Flight(){
+    }
+    public Flight(int number,String destination){
+        this.number = number;
+        this.destination = destination;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
 }
